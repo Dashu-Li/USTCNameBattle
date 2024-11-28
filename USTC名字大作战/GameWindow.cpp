da16df4dee51e94c07d9379bc2b899b47eb92cc6
Dashu Li <2928947343@qq.com>
2024-11-28 15:12:55 +0800

    增加核心功能文本框

--- USTC名字大作战/GameWindow.cpp
+++ USTC名字大作战/GameWindow.cpp
@@ -1,12 +1,12 @@
-#include "GameWindow.h"
+﻿#include "GameWindow.h"
 #include "Game.h"
 #include <QMessageBox>
 
-GameWindow::GameWindow(QWidget *parent)
+GameWindow::GameWindow(QWidget* parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
-    ui.battleEdit->setPlaceholderText("每行输入一个名字\n组队对战时用空行隔开组队");			// 设置battleEdit默认提示字
+	ui.battleEdit->setPlaceholderText("每行输入一个名字\n组队对战时用空行隔开组队");			// 设置battleEdit默认提示字
 	connect(this, &QObject::destroyed, qApp, &QApplication::quit);							// 连接游戏窗口的destroyed信号到quit槽, 以便在游戏窗口关闭时退出程序
 	connect(ui.backButton, &QPushButton::clicked, this, &GameWindow::Back);					// 连接返回按钮的clicked信号到Back信号
 	connect(ui.battleStartButton, &QPushButton::clicked, this, &GameWindow::StartBattle);	// 连接开始对战按钮的clicked信号到startBattle槽
@@ -17,19 +17,19 @@ GameWindow::~GameWindow()
 
 void GameWindow::StartBattle()
 {
-	QString battleText = ui.battleEdit->toPlainText();	// ��ȡbattleEdit���ı�
-	if (battleText.isEmpty()) {							// ����ı�Ϊ��, �򵯳���ʾ��
-		QMessageBox::warning(this, "����", "����������");
+	QString battleText = ui.battleEdit->toPlainText();	// 获取battleEdit的文本
+	if (battleText.isEmpty()) {							// 如果文本为空, 则弹出提示框
+		QMessageBox::warning(this, "错误", "请输入名字");
 		return;
 	}
-    QStringList names = battleText.split("\n", Qt::SkipEmptyParts);		// �Ի��з��ָ��ı�
-	if (names.size() < 2) {												// �����������С��2, �򵯳���ʾ��
-		QMessageBox::warning(this, "����", "������������");
+	QStringList names = battleText.split("\n", Qt::SkipEmptyParts);		// 以换行符分割文本
+	if (names.size() < 2) {												// 如果名字数量小于2, 则弹出提示框
+		QMessageBox::warning(this, "错误", "名字数量不足");
 		return;
 	}
 }
 
-void GameWindow::closeEvent(QCloseEvent *event)
+void GameWindow::closeEvent(QCloseEvent* event)
 {
 	event->accept();
 	delete this;
